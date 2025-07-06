4d040f1f93e1503d0236079c3788b3a0ec6a6af0
Abdel Rahman <hossamabdelrahman62@gmail.com>
2025-07-06 23:24:53 +0300

    refactor(exception handling): add Bad credentials message in global error handling

--- src/main/java/com/system/online_exam_system/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/system/online_exam_system/common/exceptions/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.system.online_exam_system.common.utils.ResponseUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,23 +18,31 @@ import java.util.Map;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleBadRequestException(MethodArgumentNotValidException e){
-        Map<String,String> errors = new HashMap<>();
-        for(var fieldError : e.getBindingResult().getFieldErrors()){
-            errors.put(fieldError.getField(),fieldError.getDefaultMessage());
+    public ResponseEntity<?> handleBadRequestException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        for (var fieldError : e.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
         }
-        return ResponseUtil.failure("validation failed",errors, HttpStatus.BAD_REQUEST);
+        return ResponseUtil.failure("validation failed", errors, HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<?> handleHttpMessageNotReadableException(){
-        return  ResponseUtil.failure("Missing Json object",HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> handleHttpMessageNotReadableException() {
+        return ResponseUtil.failure("Missing Json object", HttpStatus.BAD_REQUEST);
     }
+
     @ExceptionHandler({NoHandlerFoundException.class, HttpRequestMethodNotSupportedException.class})
-    public ResponseEntity<?> handlePageNotFound(){
-        return   ResponseUtil.failure("Page Not Found",HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> handlePageNotFound() {
+        return ResponseUtil.failure("Page Not Found", HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException() {
+        return ResponseUtil.failure("Bad Credentials", HttpStatus.UNAUTHORIZED);
+    }
+
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<?> handleApiException(ApiException e){
-        return  ResponseUtil.failure(e.getMessage(),e.getStatus());
+    public ResponseEntity<?> handleApiException(ApiException e) {
+        return ResponseUtil.failure(e.getMessage(), e.getStatus());
     }
 }
